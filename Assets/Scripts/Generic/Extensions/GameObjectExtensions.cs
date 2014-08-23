b17e61a55f9899764aaab89bd3ce13c1651b4eb4
Tom <tom@tomrijnbeek.nl>
2014-08-23 12:11:43 +0200

    Initial commit.

---
+++ Assets/Scripts/Generic/Extensions/GameObjectExtensions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class GameObjectExtensions
+{
+	public static T GetSafeComponent<T>(this GameObject obj) where T : MonoBehaviour
+	{
+		T component = obj.GetComponent<T>();
+		
+		if(component == null)
+		{
+			Debug.LogError("Expected to find component of type " 
+			               + typeof(T) + " but found none", obj);
+		}
+		
+		return component;
+	}
+}
